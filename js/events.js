6af1ed6ac35ce82730837267d917bb55ba601fd6
Kiley Bobbitt <jkileybob@aol.com>
2018-08-22 18:36:51 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -5,6 +5,13 @@ function getIt() {
   })
 }
 
+function frameIt() {
+  $('img').on('load', function(){
+    $('img').addClass("tasty");
+  })
+  
+}
+
 $(document).ready(function(){
 
 // call functions here
